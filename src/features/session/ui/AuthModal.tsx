970e44eec95165746e9f64278d51fbda2217cc21
qr3n <qtter85@gmail.com>
2024-12-25 21:48:30 +0700

    Initial commit

--- src/features/session/ui/AuthModal.tsx
+++ src/features/session/ui/AuthModal.tsx
@@ -1,11 +1,18 @@
-import { Modal } from "@shared/ui/modal/ui/Modal";
-import { Button } from "@shared/shadcn/components/button";
+import { useState }                                 from "react";
+import { Modal }                                    from "@shared/ui/modal/ui/Modal";
+import { Button }                                   from "@shared/shadcn/components/button";
 import { Input }                                    from "@shared/shadcn/components/input";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@shared/shadcn/components/tabs";
-import { ImageLoader } from "@shared/ui/image-loader";
-import { lockImg } from "./assets";
+import { ImageLoader }                              from "@shared/ui/image-loader";
+import { lockImg }                                  from "./assets";
+import { motion, AnimatePresence }                  from "framer-motion";
+import { InputOTP, InputOTPGroup, InputOTPSlot }    from "@shared/shadcn/components/input-otp";
 
 export const AuthModal = () => {
+    const [step, setStep] = useState(1);
+    const handleNextStep = () => setStep(2);
+    const handlePrevStep = () => setStep(1);
+
     return (
         <Modal
             trigger={<Button>Войти</Button>}
@@ -13,46 +20,86 @@ export const AuthModal = () => {
                 <ImageLoader priority src={lockImg} alt={'christmasTree'} width={24} height={24} className='w-[24px] h-[24px'/>
                 Авторизация
             </>}
-            description={'Вам придет пятизначный код'}
+            description={step === 1 ? 'Вам придет пятизначный код' : 'Введите полученный код'}
             footer={(
                 <>
-                    <Button className='w-full mt-8'>Отправить код</Button>
-                    <Button variant="outline" className='w-full mt-1 md:mt-3'>Отмена</Button>
+                    {step === 1 && (
+                        <Button className='w-full mt-8' onClick={handleNextStep}>Отправить код</Button>
+                    )}
+                    {step === 2 && (
+                        <Button className='w-full mt-8'>Подтвердить</Button>
+                    )}
+                    <Button variant="outline" className='w-full mt-1 md:mt-3' onClick={handlePrevStep}>
+                        {step === 1 ? 'Отмена' : 'Назад'}
+                    </Button>
                 </>
             )}
         >
-            <div className='px-4 md:px-0'>
-                <Tabs defaultValue={'phone'}>
-                    <TabsList className='mt-8'>
-                        <TabsTrigger value={'phone'}>Телефон</TabsTrigger>
-                        <TabsTrigger value={'email'}>Почта</TabsTrigger>
-                    </TabsList>
+            <div className='px-4 md:px-0 h-[140px]'>
+                <AnimatePresence mode="wait">
+                    {step === 1 && (
+                        <motion.div
+                            key="step-1"
+                            initial={{ opacity: 0, y: -50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 50 }}
+                            transition={{ duration: 0.15 }}
+                        >
+                            <Tabs defaultValue={'phone'}>
+                                <TabsList className='mt-5'>
+                                    <TabsTrigger value={'phone'}>Телефон</TabsTrigger>
+                                    <TabsTrigger value={'email'}>Почта</TabsTrigger>
+                                </TabsList>
 
-                    <TabsContent value={'phone'}>
-                        <div className='flex mt-6 gap-2'>
-                            <div
-                                className='bg-zinc-800 rounded-xl px-4 flex gap-2 text-sm items-center justify-center text-white'>
-                                <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6' id="flag-icon-css-ru"
-                                     viewBox="0 0 640 480">
-                                    <g fillRule="evenodd" strokeWidth="1pt">
-                                        <path fill="#fff" d="M0 0h640v480H0z"/>
-                                        <path fill="#0039a6" d="M0 160h640v320H0z"/>
-                                        <path fill="#d52b1e" d="M0 320h640v160H0z"/>
-                                    </g>
-                                </svg>
-                                +7
-                            </div>
-                            <Input type='number' placeholder='9117629553'/>
-                        </div>
-                    </TabsContent>
+                                <TabsContent value={'phone'}>
+                                    <div className='flex mt-6 gap-2'>
+                                        <div
+                                            className='bg-zinc-800 rounded-xl px-4 flex gap-2 text-sm items-center justify-center text-white'>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className='w-6 h-6' id="flag-icon-css-ru"
+                                                 viewBox="0 0 640 480">
+                                                <g fillRule="evenodd" strokeWidth="1pt">
+                                                    <path fill="#fff" d="M0 0h640v480H0z"/>
+                                                    <path fill="#0039a6" d="M0 160h640v320H0z"/>
+                                                    <path fill="#d52b1e" d="M0 320h640v160H0z"/>
+                                                </g>
+                                            </svg>
+                                            +7
+                                        </div>
+                                        <Input type='number' placeholder='9117629553'/>
+                                    </div>
+                                </TabsContent>
 
-                    <TabsContent value={'email'}>
-                        <div className='flex items-start mt-6 gap-2'>
-                            <Input type='email' label={'Почта'}/>
-                        </div>
-                    </TabsContent>
-                </Tabs>
+                                <TabsContent value={'email'}>
+                                    <div className='flex items-start mt-6 gap-2'>
+                                        <Input type='email' label={'Почта'}/>
+                                    </div>
+                                </TabsContent>
+                            </Tabs>
+                        </motion.div>
+                    )}
+
+                    {step === 2 && (
+                        <motion.div
+                            key="step-2"
+                            initial={{ opacity: 0, y: -50 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: 50 }}
+                            transition={{ duration: 0.15 }}
+                            className='flex items-center justify-center flex-col h-full'
+                        >
+                            <InputOTP maxLength={5}>
+                                <InputOTPGroup>
+                                    <InputOTPSlot index={0}/>
+                                    <InputOTPSlot index={1}/>
+                                    <InputOTPSlot index={2}/>
+                                    <InputOTPSlot index={3}/>
+                                    <InputOTPSlot index={4}/>
+                                </InputOTPGroup>
+                            </InputOTP>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
             </div>
         </Modal>
-    )
-}
+    );
+};
--- src/shared/shadcn/components/input-otp.tsx
+++ src/shared/shadcn/components/input-otp.tsx
@@ -40,7 +40,7 @@ const InputOTPSlot = React.forwardRef<
     <div
       ref={ref}
       className={cn(
-        "relative flex transition-all h-9 w-9 items-center justify-center border-y border-r border-zinc-200 text-sm shadow-sm rounded-lg dark:bg-zinc-800 dark:text-white dark:border-zinc-800",
+        "relative flex transition-all h-14 w-14 items-center justify-center border-y border-r border-zinc-200 text-2xl shadow-sm rounded-2xl font-semibold dark:bg-zinc-800 dark:text-white dark:border-zinc-800",
         isActive && "scale-110 z-10 dark:bg-zinc-700",
         className
       )}
@@ -49,7 +49,7 @@ const InputOTPSlot = React.forwardRef<
       {char}
       {hasFakeCaret && (
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center">
-          <div className="h-4 w-px animate-caret-blink bg-zinc-950 duration-1000 dark:bg-white" />
+          <div className="h-7 w-px animate-caret-blink bg-zinc-950 duration-1000 dark:bg-white" />
         </div>
       )}
     </div>
