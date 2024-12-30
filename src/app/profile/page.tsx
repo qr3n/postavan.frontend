53451216d34427ef6aa49a32ce0ac79d82ebaa54
qr3n <qtter85@gmail.com>
2024-12-30 18:42:30 +0700

    Initial commit

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,14 @@
 'use client'
 
-import { OrderCard, OrderDetailsModal } from "@entities/order";
-import { Button } from "@shared/shadcn/components/button";
-import stat from './stat.gif';
-import { ImageLoader } from "@shared/ui/image-loader";
-import { useVirtualizer } from "@tanstack/react-virtual";
-import { useRef }                       from "react";
+import { OrderCard, OrderDetailsModal }             from "@entities/order";
+import { useVirtualizer }                           from "@tanstack/react-virtual";
+import { useRef }                                   from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@shared/shadcn/components/tabs";
 
 export default function ProfilePage() {
     const orders = Array.from({ length: 1000 }); // Ваши данные
     const parentRef = useRef<HTMLDivElement>(null);
 
-    // Настройка виртуализатора
     const rowVirtualizer = useVirtualizer({
         count: orders.length,
         getScrollElement: () => parentRef.current,
@@ -21,22 +18,25 @@ export default function ProfilePage() {
     return (
         <div className="flex items-center justify-center mt-6 sm:mt-12 flex-col" vaul-drawer-wrapper="">
             <h1 className="font-semibold text-4xl sm:text-5xl">Мои заказы</h1>
-            <Button
-                variant="outline"
-                className="mt-3 sm:mt-4 flex gap-1 dark:text-zinc-100 font-normal"
-                size="sm"
-            >
-                <ImageLoader
-                    priority
-                    width={18}
-                    height={18}
-                    src={stat}
-                    alt="stat"
-                    className="w-[18px] h-[18px]"
-                />
-                Посмотреть статистику
-            </Button>
+            <Tabs defaultValue={'phone'}>
+                <TabsList className='mt-5'>
+                    <TabsTrigger value={'phone'}>Активные</TabsTrigger>
+                    <TabsTrigger value={'email'}>Запланированные</TabsTrigger>
+                    <TabsTrigger value={'closed'}>Закрытые</TabsTrigger>
+                </TabsList>
 
+                <TabsContent value={'phone'}>
+
+                </TabsContent>
+
+                <TabsContent value={'email'}>
+
+                </TabsContent>
+
+                <TabsContent value={'closed'}>
+
+                </TabsContent>
+            </Tabs>
             <div
                 className="px-4 sm:px-[50px] md:px-[100px] lg:px-[200px] xl:px-[300px] 2xl:px-[400px] mt-8 h-[calc(100dvh-240px)] sm:h-[calc(100dvh-250px)] w-full overflow-auto"
                 ref={parentRef}
--- src/entities/order/ui/OrderCard.tsx
+++ src/entities/order/ui/OrderCard.tsx
@@ -1,9 +1,25 @@
 import { Button } from "@shared/shadcn/components/button";
+import Image                         from 'next/image'
+import { ozonIcon } from "@shared/assets";
+
 
 export const OrderCard = () => {
     return (
-        <div className='bg-zinc-900 flex justify-between rounded-2xl p-3 w-full'>
-            <h1 className='text-xl font-medium'>Заказ #1</h1>
+        <div className='bg-zinc-900 flex justify-between rounded-2xl p-4 w-full'>
+            <div className='flex items-center justify-center gap-5'>
+                <Image
+                    src={ozonIcon}
+                    placeholder='blur'
+                    alt={'icon'}
+                    width={48}
+                    height={48}
+                    className='rounded-2xl object-cover h-max'
+                />
+                <div>
+                    <h1 className='text-xl font-medium'>Заказ #1</h1>
+                    <p className='text-zinc-400'>Test test test</p>
+                </div>
+            </div>
             <Button>Закрыть</Button>
         </div>
     );
