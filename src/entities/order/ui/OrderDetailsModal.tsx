8b70280ce28b7171b94491ba0ee82f3ddd707d2c
qr3n <qtter85@gmail.com>
2025-01-03 23:55:54 +0700

    Initial commit

--- src/entities/order/ui/OrderDetailsModal.tsx
+++ src/entities/order/ui/OrderDetailsModal.tsx
@@ -13,44 +13,102 @@ export const OrderDetailsModal = () => {
             description='Короб, 27 число'
             trigger={<div className='absolute top-0 left-0 w-full h-full'/>}
         >
-            <div className='h-[calc(70dvh-100px)] overflow-y-auto px-6 sm:px-0 mt-4'>
-                <div>
-                    <h1 className='text-2xl font-semibold'>Основное</h1>
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>Value</p>
+            <div className='px-6 sm:px-0'>
+                <div className='h-[calc(70dvh-100px)] overflow-y-auto mt-4'>
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
 
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>ValueValueValueValueValue</p>
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
 
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>TestTestTestTest</h2>
-                    <p className='font-medium mt-1'>Value</p>
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
 
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>ValueValueValue</p>
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
 
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>ValueValue</p>
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
+
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
+
+                    <h1 className='text-base font-medium text-white mt-8'>Что нового?</h1>
+                    <ul className='text-sm list-disc pl-6 mt-2 space-y-1'>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Бета-функции версии 2.0</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Эксклюзивная рамка для аватара</span>
+                        </li>
+                        <li className='text-blue-500'>
+                            <span className='text-[#aaa]'>Праздничная атмосфера!</span>
+                        </li>
+                    </ul>
                 </div>
-
-                <div className='mt-12'>
-                    <h1 className='text-2xl font-semibold'>Куда и откуда</h1>
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>TestTestTest</h2>
-                    <p className='font-medium mt-1'>ValueValueValueValue</p>
-
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>Value</p>
-
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>TestTest</h2>
-                    <p className='font-medium mt-1'>ValueValueValue</p>
-
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>ValueValueValueValue</p>
-
-                    <h2 className='text-zinc-400 text-xl font-light mt-4'>Test</h2>
-                    <p className='font-medium mt-1'>Value</p>
-                </div>
+                <Button className='mt-8 w-full'>Закрыть</Button>
+                <Button className='mt-4 w-full' variant='outline'>Отменить</Button>
             </div>
-            <Button className='mt-8 w-full'>Закрыть</Button>
         </Modal>
     );
 };
