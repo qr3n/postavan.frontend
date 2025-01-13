991fba7eb305665cb8e2bddfae3070577ecd9295
qren <qtter85@gmail.com>
2025-01-13 05:14:06 -0800

    test

--- src/app/order/create/page.tsx
+++ src/app/order/create/page.tsx
@@ -20,21 +20,21 @@ import { AuthModal } from "@features/session";
 
 const sliderVariants = {
     incoming: (direction: number) => ({
-        x: direction > 0 ? "100%" : "-100%", // Вход слева или справа
+        x: direction > 0 ? "100%" : "-100%",
         opacity: 0,
     }),
     active: {
-        x: 0, // Центрирование
+        x: 0,
         opacity: 1,
     },
     exit: (direction: number) => ({
-        x: direction > 0 ? "-100%" : "100%", // Уход влево или вправо
+        x: direction > 0 ? "-100%" : "100%",
         opacity: 0,
     }),
 }
 const sliderTransition = {
-    duration: 0.8, // Увеличим длительность для плавного эффекта
-    ease: [0.22, 1, 0.36, 1], // Кастомная кривая Bezier (easeOutExpo)
+    duration: 0.8,
+    ease: [0.22, 1, 0.36, 1],
 };
 
 const App = () => {
@@ -57,7 +57,7 @@ const App = () => {
     const swipeToImage = useCallback((swipeDirection: number) => {
         setImageCount(([currentIndex]) => {
             const newIndex = (currentIndex + swipeDirection + blocks.length) % blocks.length;
-            return [newIndex, swipeDirection]; // Сохраняем направление для анимации
+            return [newIndex, swipeDirection];
         });
     }, [blocks.length])
 
@@ -75,8 +75,8 @@ const App = () => {
                         className='absolute left-0 top-0 w-[10px] sm:w-[50px] md:w-[100px] lg:w-[200px] h-full bg-gradient-to-r from-black z-50 to-transparent'/>
                     <AnimatePresence initial={false} custom={direction}>
                         <motion.div
-                            key={imageCount} // Уникальный ключ для каждого слайда
-                            custom={direction} // Передаем направление для анимации
+                            key={imageCount}
+                            custom={direction}
                             variants={sliderVariants}
                             className="flex flex-col pt-4 sm:pt-12 items-center h-full absolute top-0 w-full px-8 sm:px-8"
                             initial="incoming"
