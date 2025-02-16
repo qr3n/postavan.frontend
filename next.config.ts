dc9f299c5dadb437618b22e4c2eddd5632d22f7c
qr3n <qtter85@gmail.com>
2025-02-15 23:46:44 -0800

    test

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
-import { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
     dest: "public",
-    disable: false,
-
+    disable: true
 });
 
-const nextConfig: NextConfig = {
-    reactStrictMode: false
-};
-
-export default withPWA(nextConfig)
+export default withPWA({
+    reactStrictMode: false,
+})
